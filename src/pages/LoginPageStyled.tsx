6c19c737fb316de75ee22c12ded021dbed2c8af0
AnaCatanaR <anacatana.ac@gmail.com>
2023-03-12 16:46:17 +0100

    Add LoginPageStyled

---
+++ src/pages/LoginPageStyled.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const LoginPageStyled = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export default LoginPageStyled;
